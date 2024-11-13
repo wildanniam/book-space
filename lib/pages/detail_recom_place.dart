b1fe2e448d457f2dbeb34902b054dc9a556f1fc4
wildanniam <wildanniam4@gmail.com>
2024-11-13 19:34:33 +0700

    misahin widgets

--- lib/pages/detail_recom_place.dart
+++ lib/pages/detail_recom_place.dart
@@ -105,86 +105,9 @@ class _DetailRecomPlaceState extends State<DetailRecomPlace> {
           const SizedBox(
             height: 16,
           ),
-          SizedBox(
+          const SizedBox(
             height: 650,
-            child: ListView.builder(
-              physics: const BouncingScrollPhysics(),
-              itemCount: recomPlace.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  margin: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-                  width: 300,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Stack(
-                        alignment: Alignment.bottomLeft,
-                        children: [
-                          Container(
-                            height: 200,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.rectangle,
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(10),
-                              image: DecorationImage(
-                                image: NetworkImage(
-                                    recomPlace[index]['image'] ?? ''),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          Container(
-                            height: 200,
-                            decoration: BoxDecoration(
-                              color: Colors.black.withOpacity(0.3),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      recomPlace[index]['name'] ?? '',
-                                      style: const TextStyle(
-                                        fontSize: 24,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                    Text(
-                                      recomPlace[index]['location'] ?? '',
-                                      style: const TextStyle(
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                CircleAvatar(
-                                  radius: 20,
-                                  backgroundColor: const Color(0xffF89621),
-                                  child: IconButton(
-                                    icon: const Icon(Icons.arrow_forward_ios),
-                                    color: Colors.white,
-                                    onPressed: () {},
-                                  ),
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
+            child: DetailView(),
           )
         ],
       ),
